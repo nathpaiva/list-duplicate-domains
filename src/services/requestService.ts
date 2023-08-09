65e5225b32479fb56473a2e8259d9463eadfb05d
Nath Paiva <hello@nathpaiva.com.br>
2023-08-09 16:03:59 -0700

    chore: changes data reference

--- src/services/requestService.ts
+++ src/services/requestService.ts
@@ -2,7 +2,7 @@ import { DataResponse } from '../types'
 
 export async function requestService(): Promise<DataResponse> {
   try {
-    const response = await fetch('../../data.json')
+    const response = await fetch('/assets/data.json')
     const data = await response.json()
 
     return data
